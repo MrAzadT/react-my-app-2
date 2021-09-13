63e24346cf64876c6f1a1fff5531370fb0f5acdc
azadtamal <mrazadt@gmail.com>
2021-09-13 23:55:31 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />;
+      <Navbar />;
+    </div>
+  );
 }
 
 export default App;
