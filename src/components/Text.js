78262067f73a27bd27fed5a682a328c69fddf7a7
azadtamal <mrazadt@gmail.com>
2021-09-14 21:42:26 +0600

    img

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Text(props) {
+  const handelUpClick = () => {
+    console.log("hi");
+    setTest("handel Up Click");
+  };
+  const handelOnChange = () => {
+    console.log("hy");
+  };
+  const [text, setTest] = useState("enter text Here");
+
   return (
     <div>
       <div className="mb-3 container">
         <h4>{props.heading}</h4>
-        <textarea className="form-control" id="myBox" rows="8"></textarea>
+        <textarea
+          value={text}
+          onChange={handelOnChange}
+          className="form-control"
+          id="myBox"
+          rows="8"
+        ></textarea>
+        <button onClick={handelUpClick} className="btn btn-primary mt-4">
+          Change Text
+        </button>
       </div>
     </div>
   );
