e803c2211dbaee25039daa1a4d08f53dda2a23df
azadtamal <mrazadt@gmail.com>
2021-09-15 22:59:22 +0600

    function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
-// import Text from "./components/Text";
+import Text from "./components/Text";
 function App() {
   return (
     <div>
       <Navbar title="Azad Tamal" about="About" />;
-      {/* <Text heading="enter the Text" /> */}
       <About heading="My About" />
+      <Text heading="enter the Text" />
     </div>
   );
 }
