c85369f273cc3da343b609dfaa03345bc5d738ed
azadtamal <mrazadt@gmail.com>
2021-09-15 20:35:31 +0600

    bootstarp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import Text from "./components/Text";
+// import Text from "./components/Text";
 function App() {
   return (
     <div>
       <Navbar title="Abid Azad Tamal" about="About" />;
-      <Text heading="enter the Text" />
+      {/* <Text heading="enter the Text" /> */}
+      <About />
     </div>
   );
 }
